62cccc409bd2a1e9048ed3236c9b46205496a361
Joe Cooper <josephcooper3@gmail.com>
2019-09-04 17:42:03 +0100

    completes Isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,25 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase()
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  const lettersInWord = this.word.split('');
+  let tally = 0;
+  for (let i = 0; i < lettersInWord.length; i++) {
+    letterToTest = lettersInWord[i];
+    for (letter of lettersInWord) {
+      if (letter === letterToTest) {
+        tally += 1;
+      }
+    }
+  }
+  let isogramStatus;
+  if (tally === this.word.length) {
+    isogramStatus = true;
+  } else {
+    isogramStatus = false;
+  }
+  return isogramStatus;
 }
 
 module.exports = IsogramFinder;
