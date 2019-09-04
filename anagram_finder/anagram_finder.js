2129b4940c8baeaeea7d250e4d5961b9a53c95fc
Joe Cooper <josephcooper3@gmail.com>
2019-09-04 18:13:02 +0100

    completes anagram finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,19 @@
 const AnagramFinder = function (word) {
-
+  this.word = word
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  const anagrams = [];
+  const word = this.word.toLowerCase();
+  const sortedWord = word.split('').sort();
+  otherWords.forEach ( otherWord => {
+    const lowerCaseOtherWord = otherWord.toLowerCase();
+    const sortedOtherWord = lowerCaseOtherWord.split('').sort();
+    if (sortedOtherWord.join() === sortedWord.join() && otherWord != word) {
+      anagrams.push(otherWord);
+    }
+  })
+  return anagrams;
 }
 
 module.exports = AnagramFinder;
